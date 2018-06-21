86b00cfa1e4d09a56a366193c6b3008e69fcab32
Curtis Dunne <curtdunne@gmail.com>
2018-06-21 15:58:55 -0600

    fixed bug where images fectched were too large and other small tweaks needed to make this a PWA

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,7 +5,7 @@ const Card = (props) => {
 
 	return (
 		<div className='bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
-			<img alt='ROBOTS' src={`https://robohash.org/${id}/?200x200`} />
+			<img alt='ROBOTS' src={`https://robohash.org/${id}/?size=200x200`} />
 			<div>
 				<h2>{name}</h2>
 				<p>{email}</p>
